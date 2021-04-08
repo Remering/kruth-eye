60ce5eea619541298de063915de04f18d5d7b9a4
Remering <1015488424@qq.com>
2021-04-08 20:24:53 +0800

    add log

--- src/main/kotlin/com/github/remering/krutheye/service/SessionService.kt
+++ src/main/kotlin/com/github/remering/krutheye/service/SessionService.kt
@@ -5,6 +5,7 @@ import com.github.remering.krutheye.bean.Session
 import com.github.remering.krutheye.bean.YggdrasilProfileUUID
 import com.github.remering.krutheye.config.SessionConfigurationProperties
 import com.github.remering.krutheye.entity.YggdrasilProfileEntity
+import org.slf4j.LoggerFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 
 interface SessionService {
@@ -20,6 +21,8 @@ class RedisSessionService(
     private val tokenService: TokenService,
 ): SessionService {
 
+    private val logger = LoggerFactory.getLogger(RedisSessionService::class.java)
+
     private fun getSession(serverId: String) = redisTemplate.boundHashOps<String, String>("session:$serverId")
 
     override fun join(accessToken: String, selectedProfile: YggdrasilProfileUUID, serverId: String, ip: String) {
@@ -28,6 +31,7 @@ class RedisSessionService(
         val session = Session(accessToken, selectedProfile, serverId, ip)
         sessionHash.putAll(session)
         sessionHash.expire(expireTime)
+        logger.info("Received join request from $ip with selected profile uuid = ${selectedProfile.uuid}, server id = $serverId, access token = $accessToken")
     }
 
     override fun hasJoin(username: String, serverId: String, ip: String?): YggdrasilProfileEntity? {
@@ -35,13 +39,42 @@ class RedisSessionService(
         val sessionResult = runCatching {
             Session(sessionHash.entries()!!)
         }
-        val session = sessionResult.getOrNull()?:return null
-        val profile = profileService.getByName(username)?:return null
+        val session = sessionResult.getOrNull()
+
+        if (session == null) {
+            logger.info("Rejected has join request from $ip with server id = $serverId, username = $username: No such session")
+            return null
+        }
+
+        val profile = profileService.getByName(username)
+
+        if (profile == null) {
+            logger.info("Rejected has join request from $ip with server id = $serverId, username = $username: No such profile")
+            return null
+        }
+
         var valid = if (ip == null) true else ip == session.ip
-        valid = valid && serverId == session.serverId
-        if (!valid) return null
-        val token = tokenService.getToken(session.accessToken)?:return null
-        valid = token.boundedProfile == profile.uuid
-        return if (valid) profile else null
+
+        if (!valid) {
+            logger.info("Rejected has join request from $ip with server id = $serverId, username = $username, access token = ${session.accessToken}: IP is incorrect, expect ${session.ip}")
+            return null
+        }
+
+        valid = serverId == session.serverId
+        if (!valid) {
+            logger.info("Rejected has join request from $ip with server id = $serverId, username = $username, access token = ${session.accessToken}: Server id is incorrect, expect ${session.serverId}")
+            return null
+        }
+        val token = tokenService.getToken(session.accessToken)
+        if (token == null) {
+            logger.info("Rejected has join request from $ip with server id = $serverId, username = $username, access token = ${session.accessToken}: No such access token")
+            return null
+        }
+
+        if (token.boundedProfile != profile.uuid) {
+            logger.info("Rejected has join request from $ip with server id = $serverId, username = $username, access token = ${session.accessToken}: Incorrect profile with uuid ${profile.uuid} was bounded, expect profile uuid = ${token.boundedProfile}")
+        }
+
+        return profile
     }
 }
